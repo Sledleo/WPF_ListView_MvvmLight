490aa7c614d12d6db48e9675b6a77c9ef735ecae
Leo <sledleo@hotmail.com>
2019-10-05 22:59:34 +0300

    Добавьте файлы проекта.

---
+++ WPF_ListView_MvvmLight/Book.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ListView_MvvmLight
+{
+    public class Book  : INotifyPropertyChanged
+    {
+        private int pages;
+        private string title;
+
+        public string Title { get => title; set { title = value; PCh(); } }
+
+        public int Pages { get => pages; set { pages = value; PCh(); } }
+
+
+
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void PCh([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
---
+++ WPF_ListView_MvvmLight/MainWindowModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_ListView_MvvmLight
+{
+    public class MainWindowModel 
+    {
+        public string Title { get { return "ListView - App"; } }
+
+        public ObservableCollection<Book> Books { get; private set; }
+
+        public MainWindowModel()
+        {
+            Books = new ObservableCollection<Book>
+            {
+                new Book{Title = "Qwerty", Pages = 100 },
+                new Book{Title = "Asdf", Pages = 200 }
+            };
+        }
+
+        private ICommand changeCommand;
+
+        public ICommand ChangeCommand
+        {
+            get
+            {
+                return changeCommand ?? (changeCommand = new RelayCommand( () =>
+                    {
+                        Books.Add(new Book { Title = "Zzz", Pages = 111 });
+                        Books.First().Pages += 1;
+                    }
+                ));
+            }
+        }
+
+    }
+}
